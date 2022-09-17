3ad4d2356c99bbaeb6a39ab8539c2276b7880e61
Rider <goejrider@gmail.com>
2022-09-17 13:46:54 -0500

    Created the addToCollection Function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,3 +2,20 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 
+
+//This function will take Title, Artist, and yearPublished, turn those into an object, then put that object into the collection array. It then returns the newest created object
+function addToCollection(title, artist, yearPublished){
+    let newTitle = {
+        titleName: title,
+        artistName: artist,
+        published: yearPublished
+    };
+   collection.push(title); 
+   return newTitle;
+};
+
+addToCollection('Dark Side of the Moon', 'Pink Floyd', 1973);
+
+addToCollection('Danger Days', 'My Chemical Romance', 2010);
+
+console.log(collection);
