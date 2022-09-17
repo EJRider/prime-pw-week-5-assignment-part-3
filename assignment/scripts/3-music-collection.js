2baab87fe7a20c92ec96dda026266f570b281d60
Rider <goejrider@gmail.com>
2022-09-17 15:28:20 -0500

    Added show collection function and tested

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -10,11 +10,27 @@ function addToCollection(title, artist, yearPublished){
         artistName: artist,
         published: yearPublished
     };
-   collection.push(title); 
+   collection.push(newTitle); 
    return newTitle;
 };
 
-//function showCollection()
+function showCollection(array){
+    console.log('There are', array.length, 'albums in the collection');
+    for(i=0;i<array.length;i++){
+        console.log(array[i].titleName, 'by', array[i].artistName, ', published in', array[i].published );
+    }
+}
+
+function findByArtist(artist){
+    let results = [];
+        for (let i=0; i<collection.length; i++){
+            if(artist.artistName === artist){
+                results.push([artist[i].artistName]);
+            }
+        }
+    return results;
+};
+
 
 console.log(addToCollection('Dark Side of the Moon', 'Pink Floyd', 1973));
 console.log(addToCollection('Danger Days', 'My Chemical Romance', 2010));
@@ -24,3 +40,7 @@ console.log(addToCollection('Three Cheers for Sweet Revenge', 'My Chemical Roman
 console.log(addToCollection('AAAH!BBA', 'Brian David Gilbert', 2021));
 
 console.log(collection);
+
+console.log(showCollection(collection));
+
+console.log(findByArtist('Joji'));
